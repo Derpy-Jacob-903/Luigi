75b627bc5d01bcea73ae6add424991dab90af6fb
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2024-10-16 18:16:06 -0500

    Update ModHelperData.cs
    Giving credit to Commander-Cat101 
    and warning about the XX3 Mermonkey + 010 Luigi crash

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -5,7 +5,7 @@ public static class ModHelperData
     public const string Version = "1.0.2";
     public const string Name = "Luigi";
 
-    public const string Description = "Adds Luigi to the game. Updated for v45.0";
+    public const string Description = "Adds Luigi to the game. Updated for v45" + Environment.NewLine + "Originally created by Commander-Cat101" + Environment.NewLine + "KNOWN BUG: X-X-3+ Mermonkey crashes the game when 'magically detonating' Tier 1+ Luigi's damage over time effect. THIS IS NOT DETECTABLE IN MELONLOADER/UNITY LOGS.";
 
     public const string RepoOwner = "Derpy-Jacob-903"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "Luigi-SotS"; // TODO add your repo name here, also in the download url in README.md
